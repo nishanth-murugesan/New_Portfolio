8e21b5a79c62cb9c4d04c40323d8fce26d054519
nishanth-murugesan <111063472+nishanth-murugesan@users.noreply.github.com>
2023-12-19 17:40:31 +0530

    Add files via upload

---
+++ src/utils/Themes.js
@@ -0,0 +1,22 @@
+export const darkTheme = {
+    bg:"#283194",
+    bgLight: "#283194",
+    primary:"#854CE6",
+    text_primary:"#F2F3F4",
+    text_secondary:"#f7f8fa",
+    card:"#010217",
+    card_light: '#030845',
+    button:"#854CE6",
+    white:"#FFFFFF",
+    black:"#000000",
+}
+
+export const lightTheme = {
+    bg:"#FFFFFF",
+    bgLight: "#f0f0f0",
+    primary:"#be1adb",
+    text_primary:"#111111",
+    text_secondary:"#48494a",
+    card:"#FFFFFF",
+    button:"#5c5b5b",
+}
